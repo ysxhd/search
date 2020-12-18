b07d4113e2768b29b3d6a0836ea7056094b14b10
Daisy <daisyliudesign@gmail.com>
2020-12-17 20:08:41 -0500

    加上特效

--- src/components/movie/index.js
+++ src/components/movie/index.js
@@ -5,6 +5,9 @@ import "./index.css";
 const Movie = (props) => (
   <div className="movie">
     <figure className="movie__figure">
+
+      <li>
+
       <img
         src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1608195040354&di=0a0a71578184325200e01344b6b59402&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201407%2F03%2F20140703200728_Lt2MK.jpeg"
         className="movie__poster"
@@ -17,7 +20,8 @@ const Movie = (props) => (
           <span className="movie__vote">{props.rating}</span>
       
         </figcaption>
-  
+        
+     </li>
 
       <h2 className="movie__title">{props.title} </h2>
       <p className="movie__runtime">{props.runtime} mins</p>
